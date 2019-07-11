28f0294f6ee2b5e98cdf1ca0e39c5726e022baab
Aaron Ediger <aediger@kalos-inc.com>
2019-07-11 15:34:38 -0500

    Test for loading UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,18 +17,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  
+  @ViewChild('slides', {read: IonSlides}) slides: IonSlides;
+  public swiper: any; 
   public qrHref = "";
   public hsHref = "";
   private user: Observable<firebase.User>;
   private userDetails: firebase.User = null;
   public loggedIn = false;
   functions: any;
-  
+  public DOMSlides: any; 
+  public slidesVisibility = false; 
   
   //https://stackoverflow.com/questions/53670047/angular-firebase-auth-how-to-check-if-user-is-logged-in-to-hide-login-link-from
 
-  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner, public alertController: AlertController, public actionSheetController: ActionSheetController, public network: Network, public appC: AppComponent, public el: ElementRef, @Inject(DOCUMENT) document, public af: AngularFireAuth) {
+  constructor(public navCtrl: NavController, public barcodeScanner: BarcodeScanner, public alertController: AlertController, public actionSheetController: ActionSheetController, public network: Network, public appC: AppComponent, public el: ElementRef, @Inject(DOCUMENT) document, public af: AngularFireAuth) {     
     this.user = af.authState;
     this.loggedIn = !!sessionStorage.getItem('user');
      
@@ -49,7 +51,9 @@ export class HomePage implements OnInit {
     alert("check network");
   });
   ngOnInit() {
-    
+    this.DOMSlides = document.getElementById("slides");
+    this.DOMSlides.style.visibility = "hidden"; 
+     
   }
   
    chkAuth() {
@@ -76,17 +80,44 @@ export class HomePage implements OnInit {
     this.navCtrl.navigateForward("/log-in");  
   }
   refreshBoxSlides(stopClose: Event){
-    var slides = document.getElementById("boxSlides"); 
+    // var slides = document.getElementById("slides");
+    // slides.toggleAttribute("hidden"); 
     //slides.getAttributeNames(true);
-    var a = slides.getAttributeNames; 
+    //var a = slides.getAttributeNames; 
      
-    slides.dom
-    stopClose.stopPropagation(); 
-    console.log(slides.dataset);//dom7ElementDataStorage.swiper.allowSlideNext
+    // var sl = this.el.nativeElement;
+    // sl = slides;  
+  
+    
+    console.log(stopClose);
+    stopClose.stopPropagation();
+    //console.log(slides.dataset);//dom7ElementDataStorage.swiper.allowSlideNext
     document.getElementById("refreshIcon").toggleAttribute("hidden"); 
-    document.getElementById("refreshSpinner").toggleAttribute("hidden"); 
+    document.getElementById("refreshSpinner").toggleAttribute("hidden");
+    document.getElementById("slides").toggleAttribute("hidden");
+    document.getElementById("slidesRefresh").toggleAttribute("hidden");
+     
   }
-
+  idxChng(){
+    
+    this.slides.slideTo(2); 
+    
+    console.log(this.slides); 
+    //this.slides.lockSwipes(true); 
+    this.slides.slideNext();
+  }
+  nextSlide($event) {
+    setTimeout(() => { this.slides.slideTo(1, 500); }, 500);
+  }
+  toggleSlides(){
+    if(this.slidesVisibility == true){
+      this.DOMSlides.style.visibility = "hidden"; 
+      this.slidesVisibility = false; 
+      return;
+    } 
+    this.DOMSlides.style.visibility = "visible"; 
+    this.slidesVisibility = true; 
+  }
 }
 
 
